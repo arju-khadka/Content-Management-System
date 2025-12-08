f7e7b6983dc3f36ef7e24b8f683af47977106edd
Arju Khadka <arjukhadka13@gmail.com>
2025-12-08 20:47:04 +0545

    applied mvc and mvcr concept in my website

--- app.js
+++ app.js
@@ -1,65 +1,18 @@
 const express = require("express")
-const { blogs, users } = require("./model/index")
-const { Where } = require("sequelize/lib/utils")
 require("dotenv").config()
-const { storage, multer } = require("./middleware/multerConfig")
-const { renderHome, renderAddBlog, addBlog, renderAddUser, addUSer, renderSingleBlog, renderDelete, renderUpdate, update } = require("./controller/blog/blogController")
 const app = express()
 
-const upload = multer({ storage: storage })
-
-
+const blogRoute = require("./routes/blogRoute")
 
 
 //telling nodejs to set its view engine to ejs
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: true }))
-
-
-// Home Page
-app.get("/", renderHome)
-
-app.get("/addblog", renderAddBlog)
-
-app.post("/addblog", upload.single('image'), addBlog)
-
-// user table
-app.get("/adduser", renderAddUser)
-
-app.post("/adduser", addUSer)
-
-//single blog
-app.get("/blog/:id", renderSingleBlog)
-
-//delete
-app.get("/delete/:id", renderDelete)
-
-
-//update
-app.get("/update/:id", renderUpdate)
-
-app.post("/update/:id", update)
-
 app.use(express.static("./uploads/"))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("", blogRoute)
 
 app.listen(3000, () => {
     console.log("NodeJs project has started at port 3000")
