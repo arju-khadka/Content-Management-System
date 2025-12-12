c0ae5b0ead4f61eba5aac4bf953286034fa113ab
Arju Khadka <arjukhadka13@gmail.com>
2025-12-12 20:22:39 +0545

    login pt2

--- controller/user/userController.js
+++ controller/user/userController.js
@@ -23,3 +23,30 @@ exports.addUSer = async (req, res) => {
 exports.renderLoginForm = (req,res) =>{
     res.render("login.ejs")
 }
+
+exports.loginUser = async(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.send("Please provide email and password")
+    }
+
+    // check whether the coming email user exists or not
+   const user = await users.findAll({
+        where : {
+            email:email
+        }
+    })
+    if(user.length == 0){
+        res.send("No user exist with that email")
+    }else{
+        // tyo email ko user xa vanne bujiyo --> password pani check garne
+       const isMatched = await  bcrypt.compareSync(password,user[0].password)
+       if (isMatched){
+        res.send("Login Successfully")
+       }else{
+        res.send("Invalid Password")
+       }
+
+    }
+
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
-const { renderAddUser, addUSer, renderLoginForm } = require("../controller/user/userController")
+const { renderAddUser, addUSer, renderLoginForm, loginUser } = require("../controller/user/userController")
 
 const router = require("express").Router()
 
 
 
 router.route("/adduser").get(renderAddUser).post(addUSer)
-router.route("/login").get(renderLoginForm)
+router.route("/login").get(renderLoginForm).post(loginUser)
 
 module.exports = router
